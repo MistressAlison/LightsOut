513deb8fa798a8df557f98953f34b84e1eb7b7eb
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-04 19:57:24 -0300

    Add more helper methods for PatchFactory

--- src/main/java/LightsOut/patches/PatchFactory.java
+++ src/main/java/LightsOut/patches/PatchFactory.java
@@ -4,9 +4,14 @@ package LightsOut.patches;
 import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireRawPatch;
+import com.megacrit.cardcrawl.characters.Defect;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.potions.EnergyPotion;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import javassist.*;
 
 import java.util.ArrayList;
@@ -52,6 +57,38 @@ public class PatchFactory {
         PATCHES.add(new PatchData(clazz, "posX, posY, "+r+SCALE+", "+i, "liquidColor"));
     }
 
+    public static void addRelic(Class<? extends AbstractRelic> clazz, float r, float i, Color c) {
+        PATCHES.add(new PatchData(clazz, "currentX, currentY, "+r+SCALE+", "+i, colorToString(c)));
+    }
+
+    public static void addOrb(Class<? extends AbstractOrb> clazz, float r, float i, Color c) {
+        PATCHES.add(new PatchData(clazz, "cX, cY, "+r+SCALE+", "+i, colorToString(c)));
+    }
+
+    public static void addEntity(Class<? extends AbstractCreature> clazz, String bone, float r, float i, Color c) {
+        PATCHES.add(new PatchData(clazz, "skeleton.getX() + skeleton.findBone(\""+bone+"\").getWorldX(), skeleton.getY() + skeleton.findBone(\""+bone+"\").getWorldY(), "+r+SCALE+", "+i, colorToString(c)));
+    }
+
+    public static void addSimpleVFX(Class<? extends AbstractGameEffect> clazz, float r, float i) {
+        PATCHES.add(new PatchData(clazz, "x, y, "+r+SCALE+", "+i, "color"));
+    }
+
+    public static void addColoredVFX(Class<? extends AbstractGameEffect> clazz, float r, float i, Color c) {
+        PATCHES.add(new PatchData(clazz, "x, y, "+r+SCALE+", "+i, colorToString(c)));
+    }
+
+    public static void addCard() {
+
+    }
+
+    public static void addCustom(Class<?> clazz, String xyri, Color... colors) {
+        addCustom(clazz, xyri, colorToString(colors));
+    }
+
+    public static void addCustom(Class<?> clazz, String xyri, String color) {
+        PATCHES.add(new PatchData(clazz, xyri, color));
+    }
+
     @SpirePatch(clz = CardCrawlGame.class, method = SpirePatch.CONSTRUCTOR)
     public static class AbstractCardDynamicPatch {
         @SpireRawPatch
@@ -66,7 +103,7 @@ public class PatchFactory {
             //Cards
 
             //Players
-
+            addEntity(Defect.class, "Hips_sphere", 20f, 2f, Color.CYAN);
             //Monsters
 
             //Effects
