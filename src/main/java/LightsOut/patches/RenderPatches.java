571ce75c728f237f434b612554b1354feab1432d
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-13 01:14:39 -0300

    Theory crafted patch for energy icons

--- src/main/java/LightsOut/patches/RenderPatches.java
+++ src/main/java/LightsOut/patches/RenderPatches.java
@@ -6,6 +6,7 @@ import LightsOut.util.ShaderLogic;
 import basemod.BaseMod;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 import com.evacipated.cardcrawl.mod.widepotions.potions.WidePotion;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
@@ -78,7 +79,7 @@ public class RenderPatches {
             if (shouldRenderFire())
                 ShaderLogic.lightsToRender.add(new LightData(
                         x + 15.0F * Settings.scale + ___offsetX, y + ___offsetY,
-                        ShaderLogic.TORCH_RADIUS * 2.0F, 0.08F, TITLE_COLOR));
+                        ShaderLogic.TORCH_RADIUS * 2.0F, 0.06F, TITLE_COLOR));
         }
 
         private static boolean shouldRenderFire() {
@@ -310,4 +311,44 @@ public class RenderPatches {
             }
         }
     }
+
+    /*@SpirePatch2(clz = AbstractCard.class, method = "renderSmallEnergy")
+    public static class EnergyIconLight {
+        private static final Color IC = new Color(1.0F, 0.4F, 0.1F, 1.0F);
+        private static final Color DEFECT = Color.SKY;
+        private static final Color SILENT = Color.CHARTREUSE;
+        private static final Color WATCHER = Color.PURPLE;
+        @SpirePostfixPatch
+        public static void plz(AbstractCard __instance, TextureAtlas.AtlasRegion region, float x, float y) {
+            Color c = Color.WHITE;
+            switch (__instance.color) {
+                case RED:
+                    c = IC;
+                    break;
+                case GREEN:
+                    c = SILENT;
+                    break;
+                case BLUE:
+                    c = DEFECT;
+                    break;
+                case PURPLE:
+                    c = WATCHER;
+                    break;
+                case COLORLESS:
+                case CURSE:
+                    break;
+                default:
+                    for (AbstractPlayer p : BaseMod.getModdedCharacters()) {
+                        if (p.getCardColor().equals(__instance.color)) {
+                            c = p.getCardTrailColor();
+                            break;
+                        }
+                    }
+            }
+            ShaderLogic.lightsToRender.add(new LightData(
+                    __instance.current_x + x * Settings.scale * __instance.drawScale + region.offsetX * Settings.scale + region.getRegionWidth()/2f * Settings.scale,
+                    __instance.current_y + y * Settings.scale * __instance.drawScale + region.offsetY * Settings.scale + region.getRegionHeight()/2f * Settings.scale,
+                    10f, 1f, c));
+        }
+    }*/
 }
